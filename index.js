566a952ecef42d42e636a4763e2080ded738f755
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-07 06:18:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class Passenger {
   }
 
   drivers() {
-    return this.trips().map(trip => trip.driver())
+    return this.trips().map(trip => trip.drivers())
   }
 }
 
@@ -60,3 +60,4 @@ class Trip {
 
 
 }
+ 
