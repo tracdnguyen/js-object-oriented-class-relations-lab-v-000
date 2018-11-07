a133d1d76a8f17949637a80f87e79dc388f02ea4
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-07 05:40:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverID = 0
+let passengerID = 0
+let tripID = 0
+
+class Driver(){
+  constructor(name) {
+    this.name = name
+    this.id == ++driverID
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverID === this.id
+      }.bind(this)
+    )
+  }
+
+  passengers() {
+    
+  }
+}
