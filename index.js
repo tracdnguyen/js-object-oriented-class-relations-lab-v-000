138f2a7199b01712350ed097f73db1ec2f393d2f
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-07 05:53:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ class Trip {
     store.trips.push(this)
   }
   driver() {
-    return store.drivers.find(function(driver) {
+    return store.drivers.find(driver => {
       return driver.id === this.driverId
     })
   }
